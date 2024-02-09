76adcf5f4f8ea4320ade1ad1a079668924cf43e1
hyeonbin <basilla0617@naver.com>
2024-02-09 12:11:34 +0900

    feat: JPA 사용해 Member 엔티티 생성 및 DB 저장

---
+++ JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager(); //여기에서 EntityManager를 꺼내고, 아래부터 실제 동작하는 코드를 작성
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // transaction 시작
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit(); // 성공하면 커밋
+
+        } catch (Exception e) {
+            tx.rollback(); // 문제가 생가면 롤백
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
