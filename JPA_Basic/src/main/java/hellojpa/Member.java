509dc19c6a3d4fa631df2f146f72e39be4fffaae
hyeonbin <basilla0617@naver.com>
2024-02-14 15:37:14 +0900

    feat: SEQUENCE 전략 - 매핑

--- JPA_Basic/src/main/java/hellojpa/Member.java
+++ JPA_Basic/src/main/java/hellojpa/Member.java
@@ -1,12 +1,12 @@
 package hellojpa;
 import jakarta.persistence.*;
-import jakarta.persistence.criteria.CriteriaBuilder;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
     @Column(name = "name", nullable = false)
     private String username;
