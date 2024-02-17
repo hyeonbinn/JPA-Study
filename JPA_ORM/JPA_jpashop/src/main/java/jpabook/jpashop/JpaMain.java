6c65e8314b45099e005816c7a371668d0b425662
hyeonbin <basilla0617@naver.com>
2024-02-17 14:09:32 +0900

    feat: 실전 예제 1 - 요구사항 분석과 기본 매핑 - RDB 외래키로 조회하기

--- JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 
 public class JpaMain {
 
@@ -16,6 +18,12 @@ public class JpaMain {
         tx.begin(); // transaction 시작
 
         try {
+            // 주문 조회 후 주문과 연관된 회원을 외래 키로 조회
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            // 외래 키로 다시 조회
+            Member member = em.find(Member.class, memberId);
 
             tx.commit();
 
