c7fa9eb58f16899dfe8ee3079fffc68a9647a1e1
hyeonbin <basilla0617@naver.com>
2024-03-26 17:48:00 +0900

    feat: 실전 예제 2 - 연관관계 매핑 시작

--- JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,9 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -16,6 +16,9 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    @OneToMany(mappedBy = "member") //mappedBy 속성으로 주인 지정. 즉 얘는 연관관계 주인이 아님. 읽기만 가능
+    private List<Order> orders = new ArrayList<>(); //양방향 관계 설정을 위해 1:N에서 1쪽에 List로 설정
 
     public Long getId() {
         return id;
