367df1e685768c2620db09a07646e95a6b15c9f9
hyeonbin <basilla0617@naver.com>
2024-03-26 16:43:31 +0900

    feat: 양방향 연관관계

--- JPA_ORM/JPA_jpabasic/src/main/java/hellojpa/JpaMain.java
+++ JPA_ORM/JPA_jpabasic/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -20,20 +22,35 @@ public class JpaMain {
             team.setName("TeamA");
             em.persist(team);
 
-
             //회원 저장
             Member member = new Member();
             member.setName("member1");
-            member.setTeam(team); //jpa가 team에서 pk를 꺼내서 fk로 사용함.
+            member.setTeam(team); //연관관계 주인에 값을 넣어야 함.
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam(); //팀 아이디 찾아 쓰는 게 아니라 바로 사용할 수 있음.
-            System.out.println("findTemd = " + findTeam.getName());
+            team.getMembers().add(member); //순수 객체 관계를 고려하면 사실 양쪽에 값을 모두 세팅하는 게 맞음
 
-            /**다른 팀으로 변경하고 싶을 때
-            Team newTeam = em.find(Team.class, 100L); //DB에 100이 있다고 가정했을 때.
-            findMember.setTeam(newTeam); //db에서 fk가 업데이트 됨.**/
+            /**
+             * Member member = new Member();
+* *            member.setName("member1");
+* *            em.persist(member);
+* *
+             * Team team = new Team();
+*             team.setName("TeamA");
+              team.getMembers().add(member); //이렇게하면 안됨! => TEAM_ID가 null이 됨. mappedBy이므로 읽기만 가능. 변경할 때 아예 안 봄.
+*             em.persist(team);
+             **/
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
--- JPA_ORM/JPA_jpabasic/src/main/java/hellojpa/Team.java
+++ JPA_ORM/JPA_jpabasic/src/main/java/hellojpa/Team.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +12,8 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team") //Member 클래스의 team(변수명)으로 매핑되어 있다!
+    private List<Member> members = new ArrayList<>(); //team -> member는 일대다 (OneToMany)
 
     public Long getId() {
         return id;
@@ -27,5 +29,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
