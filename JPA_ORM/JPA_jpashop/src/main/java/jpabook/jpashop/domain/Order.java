e128b01f447d3e2ce37ee02facb8ff4a0dbaa483
hyeonbin <basilla0617@naver.com>
2024-04-01 23:32:24 +0900

    feat: 실전 예제 3 - 다양한 연관관계 매핑

--- JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA_ORM/JPA_jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,12 +13,12 @@ public class Order {
     @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-
-//    @Column(name = "MEMBER_ID")
-//    private Long memberId;
     @ManyToOne
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID") //일대일관계
+    private Delivery delivery;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
