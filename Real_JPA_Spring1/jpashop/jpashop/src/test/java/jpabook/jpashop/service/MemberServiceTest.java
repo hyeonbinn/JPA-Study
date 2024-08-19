b55184d65fcc0e6f8efb56c07b760296c6698a9a
hyeonbin <basilla0617@naver.com>
2024-08-20 00:00:06 +0900

    feat : MemberService 기능 테스트 및 테스트용 설정파일 추가 (메모리 DB 사용)

---
+++ Real_JPA_Spring1/jpashop/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("park");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("park");
+
+        Member member2 = new Member();
+        member2.setName("park");
+
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) { //catch로 잡지 않으면 아래 (fail)로 넘어가 예외 발생.
+            return;
+        }
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+}
