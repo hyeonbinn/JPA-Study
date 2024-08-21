6b203b09ef12f511a6756024969e68e82ef96ca4
hyeonbin <basilla0617@naver.com>
2024-08-22 01:46:35 +0900

    feat : OrderServiceTest 작성

--- Real_JPA_Spring1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ Real_JPA_Spring1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String city;
     private String street;
     private String zipcode;
-
 }
--- Real_JPA_Spring1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Real_JPA_Spring1/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
@@ -23,7 +25,14 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
 
-//    public List<Order> findAll(Or) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+
+    }
 }
