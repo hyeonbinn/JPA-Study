36e843a1b857015f30cc7106bb447d7099609d96
hyeonbin <basilla0617@naver.com>
2024-09-27 16:45:39 +0900

    feat : 주문조회 V1: 엔티티 직접 노출

---
+++ Real_JPA_Spring2/jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.*;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * 엔티티가 변하면 API 스펙도 변함
+     * orderItem , item 관계를 직접 초기화하면 Hibernate5Module 설정에 의해 엔티티를 JSON으로 생성
+     * 응답값이 재귀적으로 무한 반환
+     * 양방향 관계 문제 발생 -> @JsonIgnore 추가
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll();
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제 초기화
+        }
+        return all;
+    }
+}
