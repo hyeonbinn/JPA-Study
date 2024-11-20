85b3f959e4b647e1b7193f2d56a1befddd60683c
hyeonbin <basilla0617@naver.com>
2024-11-20 22:15:31 +0900

    Querydsl 설정과 검증

--- QueryDSL/querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ QueryDSL/querydsl/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,43 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+		//QueryDS을 사용시 쿼리와 관련된 것을 사용할 때는 Q타입을 사용해야 함.
+		QHello qHello = QHello.hello; // Q타입 동작 확인
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		assertThat(result).isEqualTo(hello);
+
+		//lombok 동작 확인
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
+
+
+
