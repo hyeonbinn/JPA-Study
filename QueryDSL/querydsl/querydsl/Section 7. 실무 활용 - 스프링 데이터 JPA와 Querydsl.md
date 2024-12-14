85c508e6aa60af60e9aba67b6dd5e0d92624d668
hyeonbin <basilla0617@naver.com>
2024-12-14 14:04:29 +0900

    스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- QueryDSL/querydsl/querydsl/Section 7. 실무 활용 - 스프링 데이터 JPA와 Querydsl.md
+++ QueryDSL/querydsl/querydsl/Section 7. 실무 활용 - 스프링 데이터 JPA와 Querydsl.md
@@ -12,3 +12,19 @@
   - 사용자 정의 인터페이스명 + "Impl" : MemberRepositoryCustomImpl
 - MemberRepository에 JpaRepository 이외에 MemberRepositoryCustom을 상속 받아 사용하면, MemberRepository로 Spring data JPA와 Querydsl 기능을 함께 사용할 수 있게 된다.<br>
 <br>
+
+#### 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동
+- 페이징을 위해 스프링 데이터의 Page, Pageable을 Querydsl에서 활용할 수 있다.
+
+- 1. 전체 카운트를 한 번에 조회하는 단순한 방법
+  - pageable에서 offset, limit 정보를 추출해서 쿼리 조건에 추가한다.
+  - fetchResults()를 사용하면 querydsl이 알아서 totalCountquery를 날려준다. (내용과 전체 카운트를 한 번에 조회할 수 있다. (실제 쿼리가 2번 호출 된다))
+-  
+- 2. 데이터 내용과 전체 카운트를 별도로 조회하는 방법
+  - 전체 카운트를 구할 때는 조인과 같은 성능에 영향을 주는 쿼리가 필요 없을 때가 있다. 
+  - 그럴 때는 카운트 쿼리를 따로 분리하면 된다. 
+    - 1번 방법은 querydsl이 알아서 totalCountquery를 날려줬다면, 이 방법에는 totalQuery를 직접 날리는 것.
+    - 카운트 쿼리를 한 번에 조회하면, 최적화를 하지 못한다는 단점이 있다. 즉 카운트 쿼리를 최적화하고 싶다면, 별도로 날려주는 것이 좋다.
+  - +) 코드를 리팩토링해서 내용 쿼리와 전체 카운트 쿼리를 읽기 좋게 분리하면 좋다.
+
+ 
--- QueryDSL/querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ QueryDSL/querydsl/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -15,9 +19,11 @@ import static org.springframework.util.StringUtils.isEmpty;
 
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
     public MemberRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
     }
+
     @Override
     //회원명, 팀명, 나이(ageGoe, ageLoe)
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
@@ -36,6 +42,67 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         ageLoe(condition.getAgeLoe()))
                 .fetch();
     }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content,pageable,total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        /** 쿼리 자체를 분리 **/
+
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetchCount();
+        return new PageImpl<>(content, pageable, total);
+    }
+
     private BooleanExpression usernameEq(String username) {
         return isEmpty(username) ? null : member.username.eq(username);
     }
@@ -47,6 +114,4 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     private BooleanExpression ageLoe(Integer ageLoe) {
         return ageLoe == null ? null : member.age.loe(ageLoe);
     }
-
-
 }
